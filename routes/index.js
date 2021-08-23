38a397d8ecf79f8af7f656e37bb8db5345d0a9bc
Arnold C <acabang23@gmail.com>
2021-08-23 13:38:59 -0700

    set up auth.js, and worked on routers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const {csrfProtection, asyncHandler} = require ('../utils.js');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncHandler(async(req, res, next) => {
   res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+}));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const {csrfProtection, asyncHandler} = require ('../utils.js');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', asyncHandler(async(req, res, next) => {
   res.send('respond with a resource');
-});
+}));
+
+router.get('/login',csrfProtection, asyncHandler(async(req, res, next) => {
+  res.send('respond with a resource');
+}));
+
+router.post('/login',csrfProtection, asyncHandler(async(req, res, next) => {
+  res.send('respond with a resource');
+}));
 
 module.exports = router;
