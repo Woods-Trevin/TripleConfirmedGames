d830c2b149dc545696da5a8b5dea8169fa33dcb5
baselHassan <baselhassan8@gmail.com>
2021-08-23 16:04:42 -0500

    setup pug login

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,13 @@ router.get('/', asyncHandler(async(req, res, next) => {
 }));
 
 router.get('/login',csrfProtection, asyncHandler(async(req, res, next) => {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('login', { title: 'header', token: req.csrfToken()});
 }));
 
 router.post('/login',csrfProtection, asyncHandler(async(req, res, next) => {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  const { username, password } = req.body
 }));
 
 module.exports = router;
