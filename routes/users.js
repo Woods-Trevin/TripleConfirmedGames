e1d6ebba7403077fcbfa70c6cb765fafe35904ec
Rawaha M <rawaha.m@gmail.com>
2021-08-23 19:47:22 -0700

    fixed signup

--- routes/users.js
+++ routes/users.js
@@ -32,13 +32,55 @@ router.get('/signup', csrfProtection, (req, res) => {
 });
 
 const userValidators = [
-  // TODO Define the user validators.
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for First Name')
+    .isLength({ max: 50 })
+    .withMessage('First Name must not be more than 50 characters long'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Last Name')
+    .isLength({ max: 50 })
+    .withMessage('Last Name must not be more than 50 characters long'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 255 })
+    .withMessage('Email Address must not be more than 255 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 50 })
+    .withMessage('Password must not be more than 50 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    }),
 ];
 
 router.post('/signup', csrfProtection, userValidators,
   asyncHandler(async (req, res) => {
     const {
-      emailAddress,
+      email,
       firstName,
       lastName,
       username,
@@ -52,7 +94,7 @@ router.post('/signup', csrfProtection, userValidators,
 
     if (validatorErrors.isEmpty()) {
       await db.User.create({
-        emailAddress,
+        email,
         firstName,
         lastName,
         username,
@@ -61,7 +103,7 @@ router.post('/signup', csrfProtection, userValidators,
       res.redirect('/games');
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
-      // const user = db.User.build();
+      const user = db.User.build();
 
       res.render('signup', {
         title: 'Signup',
