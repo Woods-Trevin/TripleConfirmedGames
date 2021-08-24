c55077be61b9d6b259c25eed12d91e47ec6329eb
xanadel <trevinwoods41@gmail.com>
2021-08-24 11:45:34 -0500

    added required db connection

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true })
 const { check, validationResult } = require('express-validator');
+const db = require('./db/models')
 
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
