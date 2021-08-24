cf119f3841f73c83937b1440f3fc6287d7c1ee2f
Arnold C <acabang23@gmail.com>
2021-08-24 16:25:06 -0700

    working on game reviews

--- routes/games.js
+++ routes/games.js
@@ -3,7 +3,7 @@ const app = require('../app.js');
 var router = express.Router();
 const { requireAuth } = require('../auth.js');
 const db = require('../db/models');
-const { Game, Review, GameCleanRating, Shelf } = db;
+const { Game, Review, GameCleanRating, Shelf, User } = db;
 
 const { csrfProtection, asyncHandler } = require('../utils.js');
 // router.use(requireAuth); this applies to all routes, but we only want it for certain paths
@@ -20,6 +20,27 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
     include: [Review, GameCleanRating, Shelf]
   })
   const reviews = games.Reviews
+  // console.log(reviews);
+  // console.log(users)
+
+  const reviewNames = await Review.findAll(
+    {where: {
+      gameId: req.params.id
+    },
+    include: User
+  });
+
+  const userReviews = [];
+  reviewNames.forEach(element => {
+    userReviews.push(element.User.username)
+  });
+  console.log(userReviews);
+
+
+  // const userIds = reviews.map(review => review.userId);
+  // console.log(userIds);
+  // const users = await userIds.map(async id => await User.findByPk(id));
+  // console.log(users);
   res.render('game-page', {title: games.title, games: games, reviews: reviews})
 }))
 
