0dac853783d5ffcd08122920e874fa94d148daf9
xanadel <trevinwoods41@gmail.com>
2021-08-25 21:42:41 -0500

    fixing merge

--- routes/reviews.js
+++ routes/reviews.js
@@ -9,64 +9,64 @@ const { Game, Review, GameCleanRating, Shelf, User } = db;
 const { check, validationResult } = require('express-validator');
 const { csrfProtection, asyncHandler, reviewValidator } = require('../utils.js');
 
-router.get('/:id(\\d+)/edit', csrfProtection, requireAuth, asyncHandler(async(req, res) => {
-    const reviewId = parseInt(req.params.id, 10);
-    console.log(reviewId);
-    const review = await Review.findByPk(reviewId);
+router.get('/:id(\\d+)/edit', csrfProtection, requireAuth, asyncHandler(async (req, res) => {
+  const reviewId = parseInt(req.params.id, 10);
+  console.log(reviewId);
+  const review = await Review.findByPk(reviewId);
+  res.render('review-edit', {
+    title: 'Edit Review',
+    review,
+    token: req.csrfToken()
+  });
+
+}));
+
+router.post('/:id(\\d+)/edit', csrfProtection, requireAuth, reviewValidator, asyncHandler(async (req, res) => {
+  const reviewId = parseInt(req.params.id, 10);
+  const oldReview = await Review.findByPk(reviewId);
+  const { content } = req.body;
+  const username = res.locals.user.id
+  let errors = [];
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    const newReview = {
+      userId: username,
+      title: '',
+      content,
+      gameId: oldReview.gameId,
+    };
+
+    await oldReview.update(newReview)
+    res.redirect(`/games/${newReview.gameId}`);
+  } else {
+    errors = validatorErrors.array().map((error) => error.msg);
     res.render('review-edit', {
       title: 'Edit Review',
-      review,
-      token: req.csrfToken()
+      review: oldReview,
+      token: req.csrfToken(),
+      errors
     });
-
+    // const user = User.build();
+  }
+  //handle validation errors here
 }));
 
-router.post('/:id(\\d+)/edit', csrfProtection, requireAuth, reviewValidator, asyncHandler(async(req, res) => {
-    const reviewId = parseInt(req.params.id, 10);
-    const oldReview = await Review.findByPk(reviewId);
-    const {content} = req.body;
-    const username = res.locals.user.id
-    let errors = [];
-    const validatorErrors = validationResult(req);
+router.get('/:id(\\d+)/delete', csrfProtection, requireAuth, asyncHandler(async (req, res) => {
+  const reviewId = parseInt(req.params.id, 10);
+  const gameId = req.params.id;
+  console.log(req.params);
+  console.log(reviewId);
 
-    if (validatorErrors.isEmpty()) {
-        const newReview = {
-            userId: username,
-            title: '',
-            content,
-            gameId: oldReview.gameId,
-        };
+  const oldReview = await Review.findByPk(reviewId);
 
-        await oldReview.update(newReview)
-        res.redirect(`/games/${newReview.gameId}`);
-    } else {
-        errors = validatorErrors.array().map((error) => error.msg);
-        res.render('review-edit', {
-            title: 'Edit Review',
-            review: oldReview,
-            token: req.csrfToken(),
-            errors
-        });
-      // const user = User.build();
-    }
-    //handle validation errors here
-}));
-
-router.get('/:id(\\d+)/delete', csrfProtection, requireAuth, asyncHandler(async(req, res) => {
-    const reviewId = parseInt(req.params.id, 10);
-    const gameId = req.params.id;
-    console.log(req.params);
-    console.log(reviewId);
-
-    const oldReview = await Review.findByPk(reviewId);
-
-    const review = await Review.findByPk(reviewId);
-    res.render('review-delete', {
-      title: 'Delete Review',
-      review,
-      gameId: oldReview.gameId,
-      token: req.csrfToken()
-    });
+  const review = await Review.findByPk(reviewId);
+  res.render('review-delete', {
+    title: 'Delete Review',
+    review,
+    gameId: oldReview.gameId,
+    token: req.csrfToken()
+  });
 
 }));
 
@@ -79,20 +79,20 @@ router.post('/:id(\\d+)/delete', csrfProtection, requireAuth,
 
     await review.destroy();
     res.redirect(`/games/${review.gameId}`);
-}));
-
-
-router.post('/:id(\\d+)', requireAuth, asyncHandler(async(req, res) => {
-
-    const reviewLikes = ReviewLike.findAll();
-
-    
-
-    // const user = User.findAll();
-    // console.log('----------->>>>>>>>>>>>>>');
-    // console.log(user);
-
-
   }));
+
+
+router.post('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+
+  const reviewLikes = ReviewLike.findAll();
+
+
+
+  // const user = User.findAll();
+  // console.log('----------->>>>>>>>>>>>>>');
+  // console.log(user);
+  //randowm comment
+
+}));
 
 module.exports = router;
