9bd94b0edbe324210be8038602fc9a8d676f0a61
Arnold C <acabang23@gmail.com>
2021-08-26 18:48:32 -0700

    finished reviewlikes with battlescars

--- public/javascripts/reviewLikes.js
+++ public/javascripts/reviewLikes.js
@@ -8,12 +8,15 @@ window.addEventListener('DOMContentLoaded', async () => {
     const reviewLikeButton = document.querySelectorAll('.likes')
     const userId = document.querySelector('.userId').id
     const gameId = document.querySelector('.gameId').id
-    const singleReviewId = document.querySelector('.reviewId').id
+    // const singleReviewId = document.querySelector('.reviewId').id
 
     console.log(reviewLikeButton)
-    
+
     reviewLikeButton.forEach(element => {
         element.addEventListener('click', async (event) => {
+           
+            let singleReviewId = event.target.id;
+            console.log(singleReviewId);
             const likeData = await fetch(`/games/${gameId}/${singleReviewId}/`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" }
@@ -24,8 +27,8 @@ window.addEventListener('DOMContentLoaded', async () => {
             const { totalLikes, reviewId} = await likeData.json()
             const likeLength = document.querySelector(`#list${reviewId}`)
             likeLength.innerHTML = `Likes: ${totalLikes}`
-            console.log(userId)
-            console.log(totalLikes);
+            // console.log(userId)
+            // console.log(totalLikes);
         })
     });
 })
