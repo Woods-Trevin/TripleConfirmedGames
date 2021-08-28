d69415bbf5ea802fd95dd8fd87b9f24da4c35ee9
Rawaha M <rawaha.m@gmail.com>
2021-08-28 00:01:31 -0700

    updated seed files

--- db/seeders/20210823213912-addGameCleanRating.js
+++ db/seeders/20210823213912-addGameCleanRating.js
@@ -3,6 +3,26 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
    return queryInterface.bulkInsert('GameCleanRatings', [
+     {rating: 1, userId: 1, gameId: 1, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 2, userId: 2, gameId: 2, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 3, userId: 2, gameId: 3, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 4, userId: 2, gameId: 4, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 5, userId: 2, gameId: 5, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 2, userId: 2, gameId: 6, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 2, userId: 2, gameId: 7, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 3, userId: 3, gameId: 8, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 3, userId: 3, gameId: 9, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 3, userId: 3, gameId: 10, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 3, userId: 3, gameId: 11, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 3, userId: 3, gameId: 12, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 3, userId: 3, gameId: 13, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 4, userId: 3, gameId: 14, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 4, userId: 4, gameId: 15, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 4, userId: 4, gameId: 16, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 4, userId: 4, gameId: 17, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 4, userId: 4, gameId: 18, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 4, userId: 5, gameId: 19, createdAt: new Date(), updatedAt: new Date()},
+     {rating: 1, userId: 5, gameId: 20, createdAt: new Date(), updatedAt: new Date()},
 
     ], {});
   },
